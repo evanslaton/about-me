2839ace801e5a41dd924da83a43b1f7f2ae5be8a
LJ Jolley <ljjolley@gmail.com>
2018-08-09 13:38:19 -0700

    Lab 4 wrapped first question and nand name in a function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # about-me
 What do you know about me? Code Fellows 201 Lab 02
+Driver: LJ 
+Navigator: Evan
+We have worked on the code together
--- js/app.js
+++ js/app.js
@@ -10,120 +10,129 @@ var vegGuess;
 var finalMsg;
 var leastFavVeggies = ['tomato', 'eggplant', 'kale'];
 var leastFavVeggiesString = leastFavVeggies.join(', ');
+var userName;
 
 // Gets the user's name and introduces the game
-var userName = prompt('What is your name potential new friend?');
-console.log('The user\'s name: ', userName);
-alert('Hello potential new friend ' + userName + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.');
+function getName ()  {
+  userName = prompt('What is your name potential new friend?');
+  console.log('The user\'s name: ', userName);
+  alert('Hello potential new friend ' + userName + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.');
+}
+
+getName ();
 
 // Asks the user 5 y/n questions
-//Questions 1
-var answerOne = prompt('Have I ever been scuba diving?').toLowerCase();
-console.log(userName + '\'s answer to the first question:', answerOne);
-if (answerOne === 'n' || answerOne === 'no') {
-  alert('By jove you\'ve got it!');
-  correctAnswers++;
-} else {
-  alert('Wrong...');
-}
-
-// Question 2
-var answerTwo = prompt('Is my favorite beverage Diet Dr. Pepper?').toLowerCase();
-console.log(userName + '\'s answer to the second question:', answerTwo);
-if (answerTwo === 'y' || answerTwo === 'yes') {
-  alert('Heck Yes! I\'m addicted to that stuff.');
-  correctAnswers++;
-} else {
-  alert('Wrong...');
-}
-
-// Question 3
-var answerThree = prompt('Am I a twin?').toLowerCase();
-console.log(userName + '\'s answer to the third question:', answerThree);
-if (answerThree === 'y' || answerThree === 'yes') {
-  alert('Right! You\'re not just seeing double!');
-  correctAnswers++;
-} else {
-  alert('Wrong...');
-}
-
-// Question 4
-var answerFour = prompt('Do I like to travel?').toLowerCase();
-console.log(userName + '\'s answer to the fourth question:', answerFour);
-if (answerFour === 'y' || answerFour === 'yes') {
-  alert('Good answer! It\'s one of my favorite things to do.');
-  correctAnswers++;
-} else {
-  alert('Wrong...');
-}
-
-// Question 5
-var answerFive = prompt('Is my favorite color blue?').toLowerCase();
-console.log(userName + '\'s answer to the fifth question:', answerFive);
-if (answerFive === 'n' || answerFive === 'no') {
-  alert('Correct! My favorite color is red.');
-  correctAnswers++;
-} else {
-  alert('Wrong... it\'s red...');
-}
-
-// Gives the user four chances to guess the number
-while (numGuess !== num && counter < 4) {
-  numGuess = parseInt(prompt('I\'m thinking of a special number between 0 and 25. Guess what it is!'));
-  console.log(userName + '\'s guess:', numGuess);
-
-  if (numGuess === num) {
-    alert('You have the gift!');
+function questionOne ()  {
+  var answerOne = prompt('Have I ever been scuba diving?').toLowerCase();
+  console.log(userName + '\'s answer to the first question:', answerOne);
+  if (answerOne === 'n' || answerOne === 'no') {
+    alert('By jove you\'ve got it!');
     correctAnswers++;
-  } else if (counter === 3) {
-    alert('You only had one job... the number was ' + num);
-  } else if (numGuess > num) {
-    alert('Too high.');
-  } else if (isNaN(numGuess)) {
-    alert('I said to guess a NUMBER.');
   } else {
-    alert('Too low.');
-  }
-  counter++;
-}
-counter = 0;
-
-// Gives the user six chances to guess one of my least favorite vegetables
-while (guessedRight && counter < 6) {
-  counter++;
-  vegGuess = prompt('Can you guess what one of my least favorite vegetables is?').toLowerCase();
-  console.log(userName + '\'s guess:', vegGuess);
-
-  // Accounts for the user entering plural forms
-  if (vegGuess === 'tomatoes') {
-    vegGuess = 'tomato';
-  } else if (vegGuess === 'eggplants') {
-    vegGuess = 'eggplant';
-  }
-
-  for (var i = 0; i < leastFavVeggies.length; i++) {
-    if (vegGuess === leastFavVeggies[i]) {
-      alert('Right! I hate ' + leastFavVeggiesString);
-      correctAnswers++;
-      guessedRight = false;
-    }
-  }
-  if (guessedRight && counter < 6) {
-    alert('Nope, try again.');
-  } else if (counter === 6) {
-    alert('Nope, I hate ' + leastFavVeggiesString);
+    alert('Wrong...');
   }
 }
 
-if (correctAnswers === 0) {
-  finalMsg = userName + ', out of 7 questions you didn\'t get a single one right.';
-} else if (correctAnswers > 3) {
-  finalMsg = userName + ', you got a whopping ' + correctAnswers + ' out of 7 questions correct!';
-} else {
-  finalMsg = userName + ', you only got ' + correctAnswers + ' out of 7 questions correct...';
-}
+questionOne ();
 
-alert(finalMsg);
+
+// // Question 2
+// var answerTwo = prompt('Is my favorite beverage Diet Dr. Pepper?').toLowerCase();
+// console.log(userName + '\'s answer to the second question:', answerTwo);
+// if (answerTwo === 'y' || answerTwo === 'yes') {
+//   alert('Heck Yes! I\'m addicted to that stuff.');
+//   correctAnswers++;
+// } else {
+//   alert('Wrong...');
+// }
+
+// // Question 3
+// var answerThree = prompt('Am I a twin?').toLowerCase();
+// console.log(userName + '\'s answer to the third question:', answerThree);
+// if (answerThree === 'y' || answerThree === 'yes') {
+//   alert('Right! You\'re not just seeing double!');
+//   correctAnswers++;
+// } else {
+//   alert('Wrong...');
+// }
+
+// // Question 4
+// var answerFour = prompt('Do I like to travel?').toLowerCase();
+// console.log(userName + '\'s answer to the fourth question:', answerFour);
+// if (answerFour === 'y' || answerFour === 'yes') {
+//   alert('Good answer! It\'s one of my favorite things to do.');
+//   correctAnswers++;
+// } else {
+//   alert('Wrong...');
+// }
+
+// // Question 5
+// var answerFive = prompt('Is my favorite color blue?').toLowerCase();
+// console.log(userName + '\'s answer to the fifth question:', answerFive);
+// if (answerFive === 'n' || answerFive === 'no') {
+//   alert('Correct! My favorite color is red.');
+//   correctAnswers++;
+// } else {
+//   alert('Wrong... it\'s red...');
+// }
+
+// // Gives the user four chances to guess the number
+// while (numGuess !== num && counter < 4) {
+//   numGuess = parseInt(prompt('I\'m thinking of a special number between 0 and 25. Guess what it is!'));
+//   console.log(userName + '\'s guess:', numGuess);
+
+//   if (numGuess === num) {
+//     alert('You have the gift!');
+//     correctAnswers++;
+//   } else if (counter === 3) {
+//     alert('You only had one job... the number was ' + num);
+//   } else if (numGuess > num) {
+//     alert('Too high.');
+//   } else if (isNaN(numGuess)) {
+//     alert('I said to guess a NUMBER.');
+//   } else {
+//     alert('Too low.');
+//   }
+//   counter++;
+// }
+// counter = 0;
+
+// // Gives the user six chances to guess one of my least favorite vegetables
+// while (guessedRight && counter < 6) {
+//   counter++;
+//   vegGuess = prompt('Can you guess what one of my least favorite vegetables is?').toLowerCase();
+//   console.log(userName + '\'s guess:', vegGuess);
+
+//   // Accounts for the user entering plural forms
+//   if (vegGuess === 'tomatoes') {
+//     vegGuess = 'tomato';
+//   } else if (vegGuess === 'eggplants') {
+//     vegGuess = 'eggplant';
+//   }
+
+//   for (var i = 0; i < leastFavVeggies.length; i++) {
+//     if (vegGuess === leastFavVeggies[i]) {
+//       alert('Right! I hate ' + leastFavVeggiesString);
+//       correctAnswers++;
+//       guessedRight = false;
+//     }
+//   }
+//   if (guessedRight && counter < 6) {
+//     alert('Nope, try again.');
+//   } else if (counter === 6) {
+//     alert('Nope, I hate ' + leastFavVeggiesString);
+//   }
+// }
+
+// if (correctAnswers === 0) {
+//   finalMsg = userName + ', out of 7 questions you didn\'t get a single one right.';
+// } else if (correctAnswers > 3) {
+//   finalMsg = userName + ', you got a whopping ' + correctAnswers + ' out of 7 questions correct!';
+// } else {
+//   finalMsg = userName + ', you only got ' + correctAnswers + ' out of 7 questions correct...';
+// }
+
+// alert(finalMsg);
 
 
 
