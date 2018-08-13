5d0e7c559418c23b94ff3036ab83d9da0e242960
Evan Slaton <evanslaton@gmail.com>
2018-08-12 21:02:03 -0700

    Finished with lab 3 stretch goals

--- js/app.js
+++ js/app.js
@@ -10,17 +10,18 @@ var vegGuess;
 var finalMsg;
 var leastFavVeggies = ['tomato', 'eggplant', 'kale'];
 var leastFavVeggiesString = leastFavVeggies.join(', ');
+var qList = document.getElementById('questionsList');
+var aList = document.getElementById('answersList');
 
 var questions = ['What is your name potential new friend?',
   'Have I ever been scuba diving?',
   'Is my favorite beverage Diet Dr. Pepper?',
   'Am I a twin?',
   'Do I like to travel?',
-  'Is my favorite color blue?',
-
+  'Is my favorite color blue?'
 ];
 var answers = [];
-var answerCorrect = ['Hello potential new friend ' + answers[i] + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.',
+var answerCorrect = ['Hello potential new friend ' + answers[0] + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.',
   'By jove you\'ve got it!',
   'Heck Yes! I\'m addicted to that stuff.',
   'Right! You\'re not just seeing double!',
@@ -28,119 +29,72 @@ var answerCorrect = ['Hello potential new friend ' + answers[i] + '. I\'m going 
   'Correct! My favorite color is red.'
 ];
 
-for (var i = 0; i < questions.length; i++) {
-  answers[i] = prompt(questions[i]).toLowerCase();
+function newOL(list, id) {
+  var newOl= document.createElement('ol');
+  newOl.setAttribute('id', id);
+  list.appendChild(newOl);
+}
 
-  // After the user inputs their name, converts their answers into either true or false
-  if (i > 0) {
-    if (answers[i] === 'y' || answers[i] === 'yes') {
-      answers[i] = true;
-    } else {
-      answers[i] = false;
+function newLi(input, list) {
+  var newLi= document.createElement('li');
+  newLi.appendChild(document.createTextNode(input));
+  list.appendChild(newLi);
+}
+
+function firstQuestions() {
+  for (var i = 0; i < questions.length; i++) {
+    answers[i] = prompt(questions[i]).toLowerCase();
+
+    // Adds questions and answers to guessing-game.html
+    if (i > 0) {
+      newLi(questions[i], qList);
+      newLi(answers[i], aList);
     }
-  }
 
-  // Checks answers and alerts user if they were correct or not
-  if (i === 0 && answers[i]) {
-    answers[i] = answers[i].split('');
-    answers[i][0] = answers[i][0].toUpperCase();
-    answers[i] = answers[i].join('');
-    console.log(answers[i]);
-    answerCorrect[i] = 'Hello potential new friend ' + answers[i] + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.';
-    alert(answerCorrect[i]);
-  } else if ((i === 1 && answers[i] === false) || (i === 2 && answers[i] === true) || (i === 3 && answers[i] === true) || (i === 4 && answers[i] === true) || (i === 5 && answers[i] === false)) {
-    alert(answerCorrect[i]);
-    correctAnswers++;
-  } else if ((i === 1 && answers[i] === true) || (i === 2 && answers[i] === false) || (i === 3 && answers[i] === false) || (i === 4 && answers[i] === false) || ((i === 5 && answers[i] === true))) {
-    alert('Wrong...');
+    // After the user inputs their name, converts their answers into either true or false
+    if (i > 0) {
+      if (answers[i] === 'y' || answers[i] === 'yes') {
+        answers[i] = true;
+      } else {
+        answers[i] = false;
+      }
+    }
+
+    // Checks answers and alerts user if they were correct or not
+    if (i === 0 && answers[i]) {
+      answers[i] = answers[i].split('');
+      answers[i][0] = answers[i][0].toUpperCase();
+      answers[i] = answers[i].join('');
+      console.log(answers[i]);
+      answerCorrect[i] = 'Hello potential new friend ' + answers[i] + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.';
+      alert(answerCorrect[i]);
+    } else if ((i === 1 && answers[i] === false) || (i === 2 && answers[i] === true) || (i === 3 && answers[i] === true) || (i === 4 && answers[i] === true) || (i === 5 && answers[i] === false)) {
+      alert(answerCorrect[i]);
+      correctAnswers++;
+    } else if ((i === 1 && answers[i] === true) || (i === 2 && answers[i] === false) || (i === 3 && answers[i] === false) || (i === 4 && answers[i] === false) || ((i === 5 && answers[i] === true))) {
+      alert('Wrong...');
+    }
   }
 }
 
-// // Gets the user's name and introduces the game
-// function getName () {
-//   userName = prompt('What is your name potential new friend?');
-//   console.log('The user\'s name: ', userName);
-//   alert('Hello potential new friend ' + userName + '. I\'m going to ask you five yes/no questions about me. Please answer using y/n or yes/no.');
-// }
-
-// getName ();
-
-// // Asks the user 5 y/n questions
-// function questionOne () {
-//   var answerOne = prompt('Have I ever been scuba diving?').toLowerCase();
-//   console.log(userName + '\'s answer to the first question:', answerOne);
-//   if (answerOne === 'n' || answerOne === 'no') {
-//     alert('By jove you\'ve got it!');
-//     correctAnswers++;
-//   } else {
-//     alert('Wrong...');
-//   }
-// }
-
-// questionOne ();
-
-// // Question 2
-// function questionTwo () {
-//   var answerTwo = prompt('Is my favorite beverage Diet Dr. Pepper?').toLowerCase();
-//   console.log(userName + '\'s answer to the second question:', answerTwo);
-//   if (answerTwo === 'y' || answerTwo === 'yes') {
-//     alert('Heck Yes! I\'m addicted to that stuff.');
-//     correctAnswers++;
-//   } else {
-//     alert('Wrong...');
-//   }
-// }
-
-// questionTwo ();
-
-// // Question 3
-// function questionThree () {
-//   var answerThree = prompt('Am I a twin?').toLowerCase();
-//   console.log(userName + '\'s answer to the third question:', answerThree);
-//   if (answerThree === 'y' || answerThree === 'yes') {
-//     alert('Right! You\'re not just seeing double!');
-//     correctAnswers++;
-//   } else {
-//     alert('Wrong...');
-//   }
-// }
-
-// questionThree ();
-
-// // Question 4
-// function questionFour () {
-//   var answerFour = prompt('Do I like to travel?').toLowerCase();
-//   console.log(userName + '\'s answer to the fourth question:', answerFour);
-//   if (answerFour === 'y' || answerFour === 'yes') {
-//     alert('Good answer! It\'s one of my favorite things to do.');
-//     correctAnswers++;
-//   } else {
-//     alert('Wrong...');
-//   }
-// }
-
-// questionFour ();
-
-// // Question 5
-// function questionFive () {
-//   var answerFive = prompt('Is my favorite color blue?').toLowerCase();
-//   console.log(userName + '\'s answer to the fifth question:', answerFive);
-//   if (answerFive === 'n' || answerFive === 'no') {
-//     alert('Correct! My favorite color is red.');
-//     correctAnswers++;
-//   } else {
-//     alert('Wrong... it\'s red...');
-//   }
-// }
-
-// questionFive ();
+firstQuestions();
 
 //Question 6: Gives the user four chances to guess the number
 function questionSix () {
-  console.log('Random number:',num);
+
+  // Adds questions to questions list on guessing-game.html
+  newLi('I\'m thinking of a special number between 0 and 25. Guess what it is!', qList);
+  console.log('Random number:', num);
+
+  // Adds new ordered list to keep track of guesses for question 6
+  newLi('Guesses:', aList);
+  newOL(aList, 'numGuessList');
+
   while (numGuess !== num && counter < 4) {
     numGuess = parseInt(prompt('I\'m thinking of a special number between 0 and 25. Guess what it is!'));
     console.log(answers[0] + '\'s guess:', numGuess);
+
+    newLi(numGuess, numGuessList);
 
     if (numGuess === num) {
       alert('You have the gift!');
@@ -159,14 +113,22 @@ function questionSix () {
   counter = 0;
 }
 
-questionSix ();
+questionSix();
 
 // Question 7: Gives the user six chances to guess one of my least favorite vegetables
-function questionSeven () {
+function questionSeven() {
+
+  // Adds questions to questions list on guessing-game.html
+  newLi('Can you guess what one of my least favorite vegetables is?', qList);
+  newLi('Guesses:', aList);
+  newOL(aList, 'vegGuessList');
+
   while (stillGuessing && counter < 6) {
     counter++;
     vegGuess = prompt('Can you guess what one of my least favorite vegetables is?').toLowerCase();
     console.log(answers[0] + '\'s guess:', vegGuess);
+
+    newLi(vegGuess, vegGuessList);
 
     // Accounts for the user entering plural forms
     if (vegGuess === 'tomatoes') {
@@ -190,7 +152,7 @@ function questionSeven () {
   }
 }
 
-questionSeven ();
+questionSeven();
 
 // Notifies user how many correct answers they got out of total
 if (correctAnswers === 0) {
